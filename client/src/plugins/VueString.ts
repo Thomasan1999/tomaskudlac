e98474f19e322ceef8550a25ffb66d18e03eec6d
tomas <ahoj@tomaskudlac.sk>
2020-06-01 18:45:59 +0200

    Changed line breaks from CRLF to LF, added JSDoc, removed redundant and commented parts of the code, changed arguments of the replaceArray to non-optional.

--- client/src/plugins/VueString.ts
+++ client/src/plugins/VueString.ts
@@ -1,33 +1,27 @@
-import NumericRange from 'numeric-range';
 import router       from '../router';
-import store from '../store';
-import texts from '../locales';
-// import VueDate   from '../plugins/VueDate';
-// import VueNumber from '../plugins/VueNumber';
+import store        from '../store';
+import texts        from '../locales';
 
+/**
+ * @description Adds helper methods to the String prototype. To perform string operations, the string value has to be extracted from the instance of this class, similar to
+ * creating a string with new String().
+ *  */
 export default class VueString extends String
 {
+    /**
+     * @description Creates an instance of the class.
+     * @param val The string value of the instance, unlike regular String constructor, if undefined, defaults to an empty string.
+     * */
     constructor(val: string = ``)
     {
         super(val);
     }
 
-    /*
-     capitalize()
-     {
-     return this.toString() ? new VueString(`${this[0].toUpperCase()}${this.slice(1)}`) : this;
-     }
-
-     caseCamelSplit()
-     {
-     return this.toString() ? this.split(/(?=[A-Z])/).map((word) =>
-     {
-     return new VueString(word);
-     }) : [];
-     }
-     */
-
-    caseCamelTo(): VueString
+    /**
+     * @description Converts the string value to camel case.
+     * @returns A VueString instance containing the string value the method is performed on in camel case.
+     * */
+    public caseCamelTo(): VueString
     {
         return this.toString() ? new VueString(this.replace(/([_|-]\w)/g, (word) =>
         {
@@ -35,7 +29,11 @@ export default class VueString extends String
         })) : this;
     }
 
-    caseTrainTo(): VueString
+    /**
+     * @description Converts the string value to train case.
+     * @returns A VueString instance containing the string value the method is performed on in train case.
+     * */
+    public caseTrainTo(): VueString
     {
         return this.toString() ? new VueString(`${this[0].toLowerCase()}${this.slice(1).replace(/([A-Z][a-z])|([a-z][A-Z]$)/g, (match) =>
         {
@@ -48,18 +46,34 @@ export default class VueString extends String
         })}`.toLowerCase().replace(/ /g, `-`).replace(/--/g, `-`)) : this;
     }
 
-    decapitalize(): VueString
+
+    /**
+     * @description Converts the first character of the string value to lowercase.
+     * @returns A VueString instance containing the string value the method is performed on with lowercase first character.
+     * */
+    public decapitalize(): VueString
     {
         return this.toString() ? new VueString(`${this[0].toLowerCase()}${this.slice(1)}`) : this;
     }
 
-    diacriticsRemove(): VueString
+    /**
+     * @description Removes all diacritics from the string value and converts the characters to their non-diacritic counterparts, e.g. á is converted to a, ö to o, etc.
+     * @returns A VueString instance containing the string value the method is performed on without any diacritics.
+     * */
+    public diacriticsRemove(): VueString
     {
         return new VueString(this.normalize(`NFD`).replace(/[\u0300-\u036f]/g, ''));
     }
 
-    htmlParse(variables: { [s: string]: any } = {}): VueString
+    /**
+     * @description Parses the template language used in the application and returns a string to be displayed on the page.
+     * @param variables Values to replace the placeholders in the template string, variables should correlate with the types used in the template string, otherwise,
+     * unexpected behavior might occur, defaults to an empty object.
+     * @returns A VueString instance containing a parsed string value the method is performed on.
+     * */
+    public htmlParse(variables: { [s: string]: any } = {}): VueString
     {
+        /** @description Map of all values to be replaced by other values. */
         const replaceMap = {
             [`<b>`]: `<strong>`,
             [`</b>`]: `</strong>`,
@@ -68,57 +82,28 @@ export default class VueString extends String
 
         return new VueString(this.replace(/\${[^}]*}/g, (expressionRaw) =>
         {
-            const variableName = expressionRaw.replace(/^\${/, ``).replace(/}$/, ``);/* .replace(/env\.([A-Z]|_)+/, (variable) =>
-         {
-         return process.env[`VUE_APP_FM_${variable.split(`.`)[1]}`];
-         }).replace(/(storeState|storeGetters)\.([a-zA-Z]|_|\.)+/, (expressionRaw) =>
-         {
-         return expressionRaw.split(`.`).reduce((a, variableProp) =>
-         {
-         return a[variableProp];
-         }, store);
-         }); */
+            /** @description String of a placeholder between the curly brackets.  */
+            const variableName: string = expressionRaw.replace(/^\${/, ``).replace(/}$/, ``);
 
+            /** @description Type of the value, which will replace the placeholder. */
             const [variableType] = (variableName.match(/^\w+(?=\|)/) || [] as RegExpMatchArray);
 
+            /** @description The value which will replace the placeholder. */
             const result: string | undefined = (() =>
             {
                 switch (variableType)
                 {
-                    /*  case `currency`:
-                     {
-                     const [, variableOtherName] = variableName.split(`|`);
-
-                     return new VueNumber(typeof variables[variableOtherName] === `undefined` ? variableOtherName : variables[variableOtherName]).currencyFormat;
-                     }
-                     case `date`:
-                     {
-                     const [, format, date] = variableName.split(`|`);
-
-                     const dateExpr = variables[date] || date;
-
-                     return Number.isNaN(Date.parse(dateExpr)) ? `` : new VueDate(dateExpr).format(format);
-                     } */
-                    case `dictionary`:
-                    {
-                        const [, wordKey, type, variableOtherName] = variableName.split(`|`);
-                        const word = store.getters.texts.dictionary[wordKey];
-
-                        switch (type)
-                        {
-                            case `number`:
-                            {
-                                const pluralIf = new NumericRange(2, 4).includes(variables[variableOtherName]) ? word.a.pl : word.g.pl;
-                                return variables[variableOtherName] === 1 ? word.a.sg : pluralIf;
-                            }
-                            default:
-                                return;
-                        }
-                    }
                     case `href`:
                     {
-                        const [, type, route, localization, flags] = variableName.split(`|`);
+                        /**
+                         * @const type Type of the href value.
+                         * @const route The URL address the href points to.
+                         * @const locale The value which will be displayed in the link.
+                         * @const flags Flags which add extra information to how should the link be created.
+                         * */
+                        const [, type, route, locale, flags] = variableName.split(`|`);
 
+                        /** @description Prefix of the href value based on the type of the placeholder. */
                         const prefix = (() =>
                         {
                             switch (type)
@@ -132,6 +117,7 @@ export default class VueString extends String
                             }
                         })();
 
+                        /** @description The part of the href string following the prefix. */
                         const href = (() =>
                         {
                             if (type === `internal` && route)
@@ -142,18 +128,13 @@ export default class VueString extends String
                             return route;
                         })();
 
-                        // const target = type === `external` || flags?.split(`,`).includes(`_blank`) ? `target="_blank"` : ``;
+                        /** @description The target attribute of the Anchor tag.  */
                         const target = type === `external` || (flags && flags.split(`,`).includes(`_blank`) ? `target="_blank"` : ``);
-                        return `<a href="${prefix}${href}" rel="noopener noreferrer" ${target}>${localization || route}</a>`;
-                    }
-                    case `number`:
-                    {
-                        const [, value] = variableName.split(`|`);
-
-                        return new Intl.NumberFormat(`sk-SK`).format(Number(value));
+                        return `<a href="${prefix}${href}" rel="noopener noreferrer" ${target}>${locale || route}</a>`;
                     }
                     default:
                     {
+                        /** @description Variable to be parsed. */
                         const variable = variables[variableName];
 
                         if (typeof variable === `number`)
@@ -175,12 +156,22 @@ export default class VueString extends String
         })).replaceArray(Object.keys(replaceMap), Object.values(replaceMap));
     }
 
-    regexEscape(): VueString
+    /**
+     * @description Escapes any regex special characters found in the string value.
+     * @returns A VueString instance containing a regex-escaped string value the method is performed on.
+     * */
+    public regexEscape(): VueString
     {
         return new VueString(this.replace(/([-[\]{}()*+?.\\^$|#,])/g, `\\$1`));
     }
 
-    replaceArray(find: string[] = [], replace: string[] = []): VueString
+    /**
+     * @description Replaces multiple values at once.
+     * @param find Values to be replaced.
+     * @param replace Values to replace by.
+     * @returns A VueString instance containing the string value the method is performed on with the replaced values.
+     * */
+    public replaceArray(find: string[], replace: string[]): VueString
     {
         return new VueString(find.reduce((a: string, regexRaw: string, index: number) =>
         {
@@ -188,7 +179,12 @@ export default class VueString extends String
         }, this.toString()));
     }
 
-    urlTo(vueStringReturn: boolean = false): VueString | string
+    /**
+     * @description Transforms the string in to a URL-safe format.
+     * @param [vueStringReturn] Determines whether a VueString instance will be returned.
+     * @returns A VueString instance or string containing the string value the method is performed on in URL-safe format.
+     * */
+    public urlTo(vueStringReturn: boolean = false): VueString | string
     {
         const url = this.diacriticsRemove().caseTrainTo().decapitalize();
         return vueStringReturn ? url : url.toString();
