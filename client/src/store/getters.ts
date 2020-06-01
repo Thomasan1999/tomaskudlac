3722e4b65e7977c64f6ab953b1261cc549eacac9
tomas <ahoj@tomaskudlac.sk>
2020-06-01 20:12:29 +0200

    Added JSDoc, added types to arguments to improve IntelliSense.

--- client/src/store/getters.ts
+++ client/src/store/getters.ts
@@ -1,32 +1,48 @@
-import VueString from '@/plugins/VueString';
-import textsSrc  from '@/locales';
+import VueString  from '@/plugins/VueString';
+import textsSrc   from '@/locales';
+import storeState from '@/store/state';
 
 const getters = {
-    langOther({lang}): Lang
+    /** @description The language which is not active. */
+    langOther({lang}: typeof getters): Lang
     {
         return lang === `sk` ? `en` : `sk`;
     },
-    langOtherProps({navItemActive, lang, texts}, {langOther}): { href: string, nameUpper: string }
+    /** @description Properties of the language which is not active. */
+    langOtherProps({navItemActive, lang, texts}: typeof storeState, {langOther}: typeof getters): { href: string, nameUpper: string }
     {
         return {
             href: `${lang === `sk` ? `en` : ``}/#${new VueString(texts[langOther][navItemActive].title).urlTo()}`,
             nameUpper: langOther.toUpperCase()
         };
     },
-    navHeight({navSlidedDown}, {touchscreen}): number
+    /** @description Height of the nav-bar. */
+    navHeight({navSlidedDown}: typeof storeState, {touchscreen}: typeof getters): number
     {
         return ((60 + (Number(touchscreen) * 5)) + (300 * Number(navSlidedDown)));
     },
-    programmingLanguages(state): string
+    /** @description String containing all programming languages. */
+    programmingLanguages(state: typeof storeState): string
     {
         const {programmingLanguages: programmingLanguagesText} = state.texts;
 
+        /**
+         * @description Orders two programming languages by the order value.
+         * @param langProps Properties of the first programming languages.
+         * @param langProps1 Properties of the second programming languages.
+         * @returns The difference of the order values of two programming languages.
+         * */
         const compareFn = ([, langProps]: [string, ProgrammingLanguage], [, langProps1]: [string, ProgrammingLanguage]) =>
         {
             return langProps.order - langProps1.order;
         };
 
-        const textGet: (langs: {[s: string]: ProgrammingLanguage}) => string = (langs: { [s: string]: ProgrammingLanguage }) =>
+        /**
+         * @description Gets the locale of a programming language.
+         * @param langs Object containing programming languages.
+         * @returns Locale of all programming languages.
+         * */
+        const textGet: (langs: { [s: string]: ProgrammingLanguage }) => string = (langs: { [s: string]: ProgrammingLanguage }) =>
         {
             return Object.entries(langs).sort(compareFn).map(([langName, langProps]) =>
             {
@@ -36,11 +52,13 @@ const getters = {
 
         return textGet(state.programmingLanguages);
     },
-    texts({lang, texts}): typeof textsSrc.sk
+    /** @description All locales of the active language. */
+    texts({lang, texts}: typeof storeState): typeof textsSrc.sk
     {
         return texts[lang] as typeof textsSrc.sk;
     },
-    touchscreen({windowWidth}): boolean
+    /** @description Determines whether the screen is narrower than 1024px. */
+    touchscreen({windowWidth}: typeof storeState): boolean
     {
         return windowWidth < 1024;
     }
