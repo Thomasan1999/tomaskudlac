d8d956a69ed676fe5b4c93d9e38da3f5b30c5e35
tomas <ahoj@tomaskudlac.sk>
2020-06-02 16:18:24 +0200

    Added JSDoc for the webiste title.

--- client/src/locales/sk.ts
+++ client/src/locales/sk.ts
@@ -1,9 +1,9 @@
 import aboutMyself from '@/modules/about-myself/locales/sk';
-import alert from '@/modules/alert/locales/sk';
-import contact from '@/modules/contact/locales/sk';
-import footer from '@/modules/footer/locales/sk';
-import home from '@/modules/home/locales/sk';
-import portfolio from '@/modules/portfolio/locales/sk';
+import alert       from '@/modules/alert/locales/sk';
+import contact     from '@/modules/contact/locales/sk';
+import footer      from '@/modules/footer/locales/sk';
+import home        from '@/modules/home/locales/sk';
+import portfolio   from '@/modules/portfolio/locales/sk';
 
 /** @description Slovak language version. */
 export default {
@@ -13,5 +13,6 @@ export default {
     footer,
     home,
     portfolio,
+    /** @description Title of the website. */
     'title': 'Vývoj webových stránok a aplikácií'
 };
