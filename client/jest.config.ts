27318bfb76fe6df5258c93647bd0f10954959256
Tomas Kudlac <ahoj@tomaskudlac.sk>
2021-08-30 16:02:13 +0200

    feat(test): add Jest

---
+++ client/jest.config.ts
@@ -0,0 +1,11 @@
+import {InitialOptionsTsJest} from 'ts-jest/dist/types';
+import {pathsToModuleNameMapper} from 'ts-jest/utils';
+import {compilerOptions} from './tsconfig.json';
+
+const config = {
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {prefix: '<rootDir>'}),
+    preset: 'ts-jest',
+    testEnvironment: 'node'
+} as InitialOptionsTsJest;
+
+export default config;
