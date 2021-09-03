b65542025023dad86fbf71aa7470e182178bd76c
Tomas Kudlac <ahoj@tomaskudlac.sk>
2021-09-03 21:53:47 +0200

    test(router): add tests for router

---
+++ client/src/router/index.spec.ts
@@ -0,0 +1,63 @@
+import router from '@/router/index';
+import {RouteRecordRaw} from 'vue-router';
+import getMetaElement from '@/utils/getMetaElement';
+import {createPinia, setActivePinia} from 'pinia';
+import useStore from '@/store';
+import mockImageSrc from '@/mocks/mockImageSrc';
+import routes from '@/router/routes';
+import getManifestElement from '@/utils/getManifestElement';
+
+jest.mock('@/router/routes', () =>
+{
+    const routes: RouteRecordRaw[] = [
+        {
+            path: '/',
+            component: {},
+            meta: {
+                description: 'The description of the site.',
+                language: 'en',
+                title: 'The site'
+            }
+        },
+        {
+            path: '/sk',
+            component: {},
+            meta: {
+                description: 'Popis stránky.',
+                language: 'sk',
+                title: 'Stránka'
+            }
+        }
+    ];
+
+    return routes;
+});
+
+describe('router', () =>
+{
+    beforeAll(async () =>
+    {
+        mockImageSrc();
+
+        document.head.innerHTML += '<link rel="manifest">';
+
+        setActivePinia(createPinia());
+
+        const store = useStore();
+
+        await store.init();
+    });
+
+    it('syncs DOM with route metadata', async () =>
+    {
+        for await (const route of routes)
+        {
+            await router.push(route.path);
+
+            expect(document.title).toBe(route.meta!.title!);
+            expect(document.documentElement.lang).toBe(route.meta!.language!);
+            expect(getMetaElement('description').content).toBe(route.meta!.description!);
+            expect(getManifestElement().href).toContain(route.meta!.language!);
+        }
+    });
+});
