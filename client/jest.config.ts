6de82c138691bcd27677e2dd48432f5674c1accf
Tomas Kudlac <ahoj@tomaskudlac.sk>
2021-09-09 23:45:31 +0200

    fix(test): add 'html' doctype to 'vue-jest' to fix compilation error on missing attribute value

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -3,6 +3,14 @@ import {pathsToModuleNameMapper} from 'ts-jest/utils';
 import {compilerOptions} from './tsconfig.json';
 
 const config = {
+    globals: {
+        'ts-jest': {},
+        'vue-jest': {
+            pug: {
+                doctype: 'html'
+            }
+        }
+    },
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {prefix: '<rootDir>'}),
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
