2b634001e4861d51c599fb05a9611c01b13fd5a9
Thomasan1999 <ahoj@tomaskudlac.sk>
2024-08-25 18:50:10 +0200

    refactor(git): use TS for Commitlint config

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import type { UserConfig } from '@commitlint/types';
+
+const config: UserConfig = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'header-max-length': [1, 'always', 100],
     },
 };
+
+export default config;
