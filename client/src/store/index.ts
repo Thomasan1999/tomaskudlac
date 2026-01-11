b1aa06744717507ddf9ffdbbf3f785c6110ecf62
Thomasan1999 <ahoj@tomaskudlac.sk>
2026-01-11 21:41:46 +0100

    refactor(store): refactor store to use Composition API

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -4,178 +4,197 @@ import { ImageFormat, InitializingState, SiteLanguage, ToastData } from '@/store
 import { ProgrammingLanguage } from '@/store/ProgrammingLanguage';
 import dayjs from 'dayjs';
 import { defineStore } from 'pinia';
+import { ref, computed } from 'vue';
 
 /** Global store of the application. */
-const useStore = defineStore('main', {
-    actions: {
-        /** Initializes the store. Must be run before the app is mounted. Must be run only once. */
-        async init(): Promise<void> {
-            if (this.initState !== InitializingState.NOT_INITIALIZED) {
-                return;
-            }
+const useStore = defineStore('main', () => {
+    /** The scrolled page section. */
+    const activeSection = ref<string | undefined>(undefined);
+    /** The current age of Tomáš Kudláč. */
+    const age = ref<number>(null as unknown as number);
+    /** The preferred image format of images. */
+    const imageFormat = ref(ImageFormat.WEBP);
+    const initState = ref(InitializingState.NOT_INITIALIZED);
+    const language = ref(SiteLanguage.SK);
+    const locales = ref(null as unknown as typeof skLocales | typeof enLocales);
+    const navbarHeight = ref(60);
+    const programmingLanguages = ref<ProgrammingLanguage[]>(
+        [
+            {
+                children: [
+                    {
+                        home: true,
+                        title: 'Vue.js',
+                    },
+                    {
+                        home: true,
+                        title: 'TypeScript',
+                    },
+                    {
+                        home: true,
+                        title: 'Node.js',
+                    },
+                ],
+                home: true,
+                title: 'JS',
+            },
+            {
+                an: true,
+                home: true,
+                title: 'HTML',
+            },
+            {
+                children: [
+                    {
+                        title: 'Stylus',
+                    },
+                    {
+                        title: 'SCSS',
+                    },
+                    {
+                        title: 'Less',
+                    },
+                ],
+                home: true,
+                title: 'CSS',
+            },
+            {
+                children: [
+                    {
+                        home: true,
+                        title: 'PostgreSQL',
+                    },
+                ],
+                title: 'SQL',
+            },
+            {
+                children: [
+                    {
+                        home: true,
+                        title: 'MongoDB',
+                    },
+                ],
+                title: 'NoSQL',
+            },
+            {
+                home: true,
+                title: 'PHP',
+            },
+        ].map((language) => new ProgrammingLanguage(language)),
+    );
+    const scrollbarWidth = ref(window.innerWidth > 1023 ? 17 : 0);
+    const toasts = ref<ToastData[]>([]);
+    const windowHeight = ref(window.innerHeight);
+    const windowWidth = ref(window.innerWidth);
 
-            this.initState = InitializingState.INITIALIZING;
+    const isTouchscreen = computed<boolean>(() => {
+        return windowWidth.value < 1024;
+    });
 
-            await this.initListeners();
-            await this.updateAge();
-            await this.initImageFormat();
+    /** List of programming languages joined by commas. */
+    const programmingLanguagesString = computed<string>(() => {
+        return programmingLanguages.value.map((language) => language.toString()).join(', ');
+    });
 
-            this.initState = InitializingState.INITIALIZED;
-        },
-        addToast(toast: ToastData): void {
-            this.toasts.push(toast);
-        },
-        /** Determines the preferred image format of all images. Checks if WebP is supported, if not, JPEG is used. */
-        async getImageFormat(): Promise<ImageFormat> {
-            return new Promise((resolve) => {
-                const webP = new Image();
-                webP.onload = webP.onerror = function () {
-                    resolve(webP.height === 2 ? ImageFormat.WEBP : ImageFormat.JPG);
-                };
-                webP.src =
-                    'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-            });
-        },
-        /** Initializes the function which checks for the preferred image format. */
-        async initImageFormat(): Promise<void> {
-            let imageFormat = localStorage.getItem('imageFormat') as ImageFormat | null;
+    /** Determines the preferred image format of all images. Checks if WebP is supported, if not, JPEG is used. */
+    const getImageFormat = async (): Promise<ImageFormat> => {
+        return new Promise((resolve) => {
+            const webP = new Image();
+            webP.onload = webP.onerror = function () {
+                resolve(webP.height === 2 ? ImageFormat.WEBP : ImageFormat.JPG);
+            };
+            webP.src =
+                'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+        });
+    };
 
-            if (!imageFormat) {
-                imageFormat = await this.getImageFormat();
-                localStorage.setItem('imageFormat', imageFormat);
-            }
+    /** Initializes the function which checks for the preferred image format. */
+    const initImageFormat = async (): Promise<void> => {
+        let format = localStorage.getItem('imageFormat') as ImageFormat | null;
 
-            this.imageFormat = imageFormat;
-        },
-        /** Initializes global event listeners. */
-        async initListeners(): Promise<void> {
-            this.onWindowResize = this.onWindowResize.bind(this);
+        if (!format) {
+            format = await getImageFormat();
+            localStorage.setItem('imageFormat', format);
+        }
 
-            window.addEventListener('resize', this.onWindowResize);
-        },
-        removeToast(toastIndex: number): void {
-            this.toasts.splice(toastIndex, 1);
-        },
-        async onWindowResize(): Promise<void> {
-            this.windowHeight = window.innerHeight;
-            this.windowWidth = window.innerWidth;
-        },
-        /** Updates the current age. Fired on store initialization and birthday. */
-        async updateAge(): Promise<void> {
-            /** @description Birth date of Tomáš Kudláč. */
-            const birthDate: string = '1999-06-30T08:30:00+02:00';
+        imageFormat.value = format;
+    };
 
-            /** @description The current timestamp. */
-            const now: dayjs.Dayjs = dayjs();
+    const onWindowResize = async (): Promise<void> => {
+        windowHeight.value = window.innerHeight;
+        windowWidth.value = window.innerWidth;
+    };
 
-            this.age = now.diff(birthDate, 'y');
+    /** Initializes global event listeners. */
+    const initListeners = async (): Promise<void> => {
+        window.addEventListener('resize', onWindowResize);
+    };
 
-            const nextBirthday = dayjs(birthDate).add(this.age + 1, 'y');
+    /** Updates the current age. Fired on store initialization and birthday. */
+    const updateAge = async (): Promise<void> => {
+        /** Birth date of Tomáš Kudláč. */
+        const birthDate: string = '1999-06-30T08:30:00+02:00';
 
-            const timeUntilNextBirthday = nextBirthday.diff(now);
+        /** The current timestamp. */
+        const now: dayjs.Dayjs = dayjs();
 
-            // prevent using higher value than allowed by Node.js
-            if (timeUntilNextBirthday > 2 ** 32) {
-                return;
-            }
+        age.value = now.diff(birthDate, 'y');
 
-            setTimeout(() => {
-                this.updateAge();
-            }, timeUntilNextBirthday);
-        },
-    },
-    getters: {
-        initialized(): boolean {
-            return this.initState === InitializingState.INITIALIZED;
-        },
-        isTouchscreen(): boolean {
-            return this.windowWidth < 1024;
-        },
-        /** List of programming languages joined by commas. */
-        programmingLanguagesString(): string {
-            return this.programmingLanguages.map((language) => language.toString()).join(', ');
-        },
-    },
-    state() {
-        return {
-            /** The scrolled page section. */
-            activeSection: undefined as string | undefined,
-            /** The current age of Tomáš Kudláč. */
-            age: null as unknown as number,
-            /** The preferred image format of images. */
-            imageFormat: ImageFormat.WEBP,
-            initState: InitializingState.NOT_INITIALIZED,
-            language: 'sk' as SiteLanguage,
-            locales: null as unknown as typeof skLocales | typeof enLocales,
-            navbarHeight: 60,
-            programmingLanguages: [
-                {
-                    children: [
-                        {
-                            home: true,
-                            title: 'Vue.js',
-                        },
-                        {
-                            home: true,
-                            title: 'TypeScript',
-                        },
-                        {
-                            home: true,
-                            title: 'Node.js',
-                        },
-                    ],
-                    home: true,
-                    title: 'JS',
-                },
-                {
-                    an: true,
-                    home: true,
-                    title: 'HTML',
-                },
-                {
-                    children: [
-                        {
-                            title: 'Stylus',
-                        },
-                        {
-                            title: 'SCSS',
-                        },
-                        {
-                            title: 'Less',
-                        },
-                    ],
-                    home: true,
-                    title: 'CSS',
-                },
-                {
-                    children: [
-                        {
-                            home: true,
-                            title: 'PostgreSQL',
-                        },
-                    ],
-                    title: 'SQL',
-                },
-                {
-                    children: [
-                        {
-                            home: true,
-                            title: 'MongoDB',
-                        },
-                    ],
-                    title: 'NoSQL',
-                },
-                {
-                    home: true,
-                    title: 'PHP',
-                },
-            ].map((language) => new ProgrammingLanguage(language)) as ProgrammingLanguage[],
-            scrollbarWidth: window.innerWidth > 1023 ? 17 : 0,
-            toasts: [] as ToastData[],
-            windowHeight: window.innerHeight,
-            windowWidth: window.innerWidth,
-        };
-    },
+        const nextBirthday = dayjs(birthDate).add(age.value + 1, 'y');
+
+        const timeUntilNextBirthday = nextBirthday.diff(now);
+
+        // prevent using higher value than allowed by Node.js
+        if (timeUntilNextBirthday > 2 ** 32) {
+            return;
+        }
+
+        setTimeout(() => {
+            updateAge();
+        }, timeUntilNextBirthday);
+    };
+
+    /** Initializes the store. Must be run before the app is mounted. Must be run only once. */
+    const init = async (): Promise<void> => {
+        if (initState.value !== InitializingState.NOT_INITIALIZED) {
+            return;
+        }
+
+        initState.value = InitializingState.INITIALIZING;
+
+        await initListeners();
+        await updateAge();
+        await initImageFormat();
+
+        initState.value = InitializingState.INITIALIZED;
+    };
+
+    const addToast = (toast: ToastData): void => {
+        toasts.value.push(toast);
+    };
+
+    const removeToast = (toastIndex: number): void => {
+        toasts.value.splice(toastIndex, 1);
+    };
+
+    return {
+        activeSection,
+        addToast,
+        age,
+        imageFormat,
+        init,
+        isTouchscreen,
+        language,
+        locales,
+        navbarHeight,
+        programmingLanguages,
+        programmingLanguagesString,
+        removeToast,
+        scrollbarWidth,
+        toasts,
+        windowHeight,
+        windowWidth,
+    };
 });
 
 export default useStore;
