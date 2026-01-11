64461f9b7753cc7eec698e67b2edb81720492f73
Thomasan1999 <ahoj@tomaskudlac.sk>
2026-01-11 18:22:28 +0100

    feat(contact): change phone number to Czech one

--- client/src/components/main/contact/ContactBriefText.vue
+++ client/src/components/main/contact/ContactBriefText.vue
@@ -5,6 +5,9 @@
 
     const store = useStore();
 
+    const phoneNumber = '+420705958382';
+    const formattedPhoneNumber = phoneNumber.replace(/^(\+\d{3})(\d{3})(\d{3})(\d{3})$/, '$1 $2 $3 $4');
+
     const locales = computed(() => store.locales.sections.contact);
 
     const briefText = computed(() => locales.value.briefText);
@@ -22,9 +25,9 @@
             <a
                 data-testid="phone-number-link"
                 class="hover:text-text-highlight underline transition-colors"
-                href="tel:+421949353783"
+                :href="`tel:${phoneNumber}`"
                 :title="phoneTitle"
-                >+421 949 353 783</a
+                >{{ formattedPhoneNumber }}</a
             >
         </template>
     </LocalesTextParser>
